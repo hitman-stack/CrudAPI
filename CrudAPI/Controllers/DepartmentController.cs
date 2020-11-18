103fe9dc82bc682be2d44a1e531ae9ead6ae26eb
hitman-stack <45466709+hitman-stack@users.noreply.github.com>
2020-11-19 02:14:20 +0530

    Adding Controller for Department
    - Controller for Department with all the Rest Methods
    - Enabling Cors

---
+++ CrudAPI/Controllers/DepartmentController.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using CrudAPI.Controllers.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace CrudAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public DepartmentController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        [HttpGet]
+        public JsonResult Get()
+        {
+            var query = @"select DepartmentId, DepartmentName from dbo.Department";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+            SqlDataReader myReader;
+            using (var myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myCon.Close();
+                }
+            }
+            return new JsonResult(table);
+
+
+        }
+
+        [HttpPost]
+        public JsonResult Post(Department department)
+        {
+
+            var query = @"insert into dbo.Department values
+                        ('" + department.DepartmentName + @"')
+                ";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+            SqlDataReader myReader;
+            using (var myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myCon.Close();
+                }
+            }
+            return new JsonResult("Added Successfully!");
+        }
+        [HttpPut]
+        public JsonResult Put(Department department)
+        {
+
+            var query = @"
+                        update dbo.Department set
+                        DepartmentName='" + department.DepartmentName + @"'
+                        where DepartmentId= " + department.DepartmentId + @"
+                ";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+            SqlDataReader myReader;
+            using (var myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myCon.Close();
+                }
+            }
+            return new JsonResult("Updated Successfully!");
+        }
+
+        [HttpDelete("{id}")]
+        public JsonResult Delete(int id )
+        {
+
+            var query = @"
+                        delete from dbo.Department
+                        where DepartmentId= " + id + @"
+                ";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+            SqlDataReader myReader;
+            using (var myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myCon.Close();
+                }
+            }
+            return new JsonResult("Deleted  Successfully!");
+        }
+    }
+}
